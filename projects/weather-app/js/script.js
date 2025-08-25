782fd88d1d36ac632a0fd0f6954427b4a81c5339
James Hemsworth <66384597+JamesHemi@users.noreply.github.com>
2025-08-25 14:42:46 -0400

    Update script.js

--- projects/weather-app/js/script.js
+++ projects/weather-app/js/script.js
@@ -2,7 +2,7 @@ async function getWeather() {
     const city = document.getElementById('cityInput').value.trim();
     if (city === '') return alert("Please enter a city name.");
 
-   const url = 'https://api.weatherstack.com/current?access_key={b4cad932ef8c1cf44d97b03517f59b71}&query=fetch:ip';
+   const url = 'https://api.weatherstack.com/current?access_key={b4cad932ef8c1cf44d97b03517f59b71}&query=New York&forecast_days=7';
 const options = {
 	method: 'GET'
 };
@@ -14,3 +14,5 @@ try {
 } catch (error) {
 	console.error(error);
 }
+
+
